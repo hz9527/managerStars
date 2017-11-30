119f5bbf77814afc0e292219bf39301e05016903
huangzhong <huangzhong@daojia.com>
2017-11-30 19:11:26 +0800

    :tada: init

---
+++ lib/app/src/devPages/index.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">
+    test
+  </div>
+</template>
+
+<script>
+import {getUser} from '../API/index'
+export default {
+  created () {
+    this.init()
+  },
+  methods: {
+    init () {
+      console.log(getUser)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
